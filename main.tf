5214e6b871a81c2d74695b8c8a4cb249e2491c50
Emil Engfors <emil.engfors@devoteam.com>
2020-09-17 08:46:33 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,28 +4,28 @@ module "okta" {
   groups  = var.oktaasa_groups
 }
 
-module "network" {
-  source  = "terraform-google-modules/network/google"
-  version = "2.5.0"
+# module "network" {
+#   source  = "terraform-google-modules/network/google"
+#   version = "2.5.0"
 
-  network_name = "showcase-demo-vpc"
-  project_id   = var.gcp_project
+#   network_name = "showcase-demo-vpc"
+#   project_id   = var.gcp_project
 
-  subnets = [
-    {
-      subnet_name   = "subnet-01"
-      subnet_ip     = "10.0.1.0/24"
-      subnet_region = var.gcp_region
-    },
-    {
-      subnet_name   = "subnet-02"
-      subnet_ip     = "10.0.2.0/24"
-      subnet_region = var.gcp_region
-      # subnet_private_access = "true"
-      # subnet_flow_logs      = "true"
-    },
-  ]
-}
+#   subnets = [
+#     {
+#       subnet_name   = "subnet-01"
+#       subnet_ip     = "10.0.1.0/24"
+#       subnet_region = var.gcp_region
+#     },
+#     {
+#       subnet_name   = "subnet-02"
+#       subnet_ip     = "10.0.2.0/24"
+#       subnet_region = var.gcp_region
+#       # subnet_private_access = "true"
+#       # subnet_flow_logs      = "true"
+#     },
+#   ]
+# }
 
 # module "instances" {
 #   source           = "./instances"
@@ -60,7 +60,7 @@ resource "google_compute_instance" "target" {
     terraform   = true
   }
   network_interface {
-    network    = module.network.network_name
+    network    = "terraform-vpc-network"
     subnetwork = "subnet-01"
   }
 }
